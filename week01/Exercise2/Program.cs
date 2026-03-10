1bc420ebddffbf958ce56d48b906a366198f7cb8
wpechel-hub <wpechel@gmail.com>
2026-03-09 23:16:21 -0600

    Finish Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Runtime.Intrinsics.X86;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
-}
+        Console.Write("What is your grade percent? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letter = "";
+
+
+        if (grade >= 90)
+        {
+            letter = "A"; 
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";  
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";  
+        }
+
+         else if (grade >= 60)
+        {
+            letter = "D";  
+        }
+
+         else
+        {
+           letter = "F";  
+        }
+
+       
+        
+        Console.WriteLine($"You have earned the grade: {letter} ");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry, you haven't passed the course, please contact your instructor.");
+        }
+    } 
+
+}   
